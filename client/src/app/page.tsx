8bfb9fbfd0612f7ef3ae2d6ff3561072e84a244e
abdelrhman osama <abdalrhman.osama.mostafa@gmail.com>
2026-07-24 21:19:11 +0300

    1

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   const router = useRouter();
 
   const isLoggedIn = async () => {
-    const res = await fetch("http://localhost:5000/users/me", {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
       method: "GET",
       credentials: "include",
     });
